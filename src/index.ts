cc05c2b9056900fbe3c8d8744b7d3f80c3a2da35
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-03-14 20:50:31 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Carats Core
+ * Advanced Frontend Architecture
+ */
+
+export const init = () => {
+  console.log('Stay tuned for the next update of Carats Core!');
+};
+
+// Gelecekteki ana framework bileşenleri burada tanımlanacak
