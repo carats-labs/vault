c69b0446715d7cf8d00bfc65720964ee8f728ebb
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-03-25 02:08:40 +0300

    fix server loader middleware

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -1,12 +1,12 @@
-import { findClosest } from '@carats/core';
-import { CaratsServerEntry } from '@carats/ssr';
-import { NextFunction, Request, Response, Router } from 'express';
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { ViteDevServer } from 'vite';
+import { findClosest } from '@carats/core'
+import { CaratsServerEntry } from '@carats/ssr'
+import { NextFunction, Request, Response, Router } from 'express'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { ViteDevServer } from 'vite'
 
-export const router: Router = Router();
-const loader = (path: string) => isProduction ? import(path) : vite.ssrLoadModule(path);
+export const router: Router = Router()
+const loader = (path: string) => isProduction ? import(path) : vite.ssrLoadModule(path)
 
 const isProduction = process.env.NODE_ENV === 'production'
 const config = {
@@ -16,14 +16,14 @@ const config = {
   }
 }
 
-const _clientBaseDir = isProduction ? 'dist/client' : 'src/client';
-const clientBase = findClosest(_clientBaseDir);
-if (!clientBase) throw new Error('Can not find client base directory: ' + _clientBaseDir);
+const _clientBaseDir = isProduction ? 'dist/client' : 'src/client'
+const clientBase = findClosest(_clientBaseDir)
+if (!clientBase) throw new Error('Can not find client base directory: ' + _clientBaseDir)
 
-const _serverBaseDir = isProduction ? 'dist/server' : 'src/server';
-const serverBase = findClosest(_serverBaseDir);
+const _serverBaseDir = isProduction ? 'dist/server' : 'src/server'
+const serverBase = findClosest(_serverBaseDir)
 
-if (!serverBase) throw new Error('Can not find server base directory: ' + _serverBaseDir);
+if (!serverBase) throw new Error('Can not find server base directory: ' + _serverBaseDir)
 
 const templateHtml = isProduction
   ? readFileSync(join(clientBase, 'index.html'), 'utf-8')
@@ -35,7 +35,7 @@ if (!isProduction) {
   vite = await createServer({
     configFile: join(serverBase, '../..', 'vite.config.client.ts'),
   })
-  Object.assign(config, vite.config);
+  Object.assign(config, vite.config)
   router.use(vite.middlewares)
 } else {
   const compression = (await import('compression')).default
@@ -47,48 +47,50 @@ if (!isProduction) {
 declare global {
   namespace Express {
     interface Request {
-      serverEntry: CaratsServerEntry;
+      serverEntry: CaratsServerEntry
     }
   }
 }
 
 const serverLoader = async (req: Request, _res: Response, next: NextFunction) => {
-  req.serverEntry = await loader(join(serverBase, 'entrypoint'));
-  next();
-};
+  req.serverEntry = await loader(join(serverBase, 'entrypoint'))
+  next()
+}
+
+router.use(serverLoader)
 
 router.get('/.carats/hallmarks', async (req: Request, res: Response) => {
-  res.json(Object.keys(req.serverEntry.hallmarks));
-});
+  res.json(Object.keys(req.serverEntry.hallmarks))
+})
 
 router.all('*splat', serverLoader, async (req: Request, res: Response) => {
   try {
-    const url = req.originalUrl.replace(config.base, '/');
+    const url = req.originalUrl.replace(config.base, '/')
     const caratsRequest = {
       url,
       headers: req.headers as Record<string, string>,
       cookies: req.cookies,
       method: req.method,
       data: req.body,
-    };
-    const { getServerProps, render } = req.serverEntry;
+    }
+    const { getServerProps, render } = req.serverEntry
     if (url.startsWith('/api')) {
-      const data = await getServerProps(caratsRequest);
-      const { _status, ...payload } = data;
-      return res.status(_status || 200).json(payload);
+      const data = await getServerProps(caratsRequest)
+      const { _status, ...payload } = data
+      return res.status(_status || 200).json(payload)
     }
 
     let template = isProduction
       ? templateHtml
       : await vite.transformIndexHtml(url, readFileSync(join(clientBase, 'index.html'), 'utf-8'))
 
-    const { head = '', html = '' } = await render(caratsRequest);
+    const { head = '', html = '' } = await render(caratsRequest)
 
     const result = template
       .replace(`<!--app-head-->`, head)
       .replace(`<!--app-html-->`, html)
 
-    res.status(200).set({ 'Content-Type': 'text/html' }).send(result);
+    res.status(200).set({ 'Content-Type': 'text/html' }).send(result)
   }
   catch (e) {
     if (e instanceof Error) {
@@ -100,4 +102,4 @@ router.all('*splat', serverLoader, async (req: Request, res: Response) => {
       res.status(500).end(JSON.stringify(e))
     }
   }
-});
+})
