20ebbaab96b6c6e07d851697dbf1761e6385e95d
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-03-24 16:47:10 +0300

    refactor: seperate render module

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,6 +1,4 @@
-import { matchRoute } from "@carats/url";
 import { existsSync } from "fs";
-import { transpile } from "jjsx";
 import { createRequire } from "module";
 import { basename, join } from "path";
 import { cwd } from "process";
@@ -119,52 +117,4 @@ export function getConfig(): CaratsConfig {
 
   confCache = defaults;
   return confCache;
-}
-
-export interface CaratsRenderContext {
-  inAppRouting?: boolean;
-  routes: Record<string, CaratsComponent>
-  suspense: {
-    loading: () => JSX.Element;
-    error: (error: Error) => JSX.Element;
-    notFound: () => JSX.Element;
-  }
-}
-
-interface PageComponentResult {
-  component: CaratsComponent<any>;
-  params: Record<string, string>;
-}
-
-export function getPageComponent(this: CaratsRenderContext, path: string): PageComponentResult {
-
-  const { routes, suspense } = this;
-
-
-  for (const routePath in routes) {
-    const component = routes[routePath];
-    const matchedParams = matchRoute(routePath, path);
-    if (matchedParams) {
-      if (component instanceof Promise) {
-        const SuspenseComponent = () => {
-          const suspenseId = `carats-suspense-${routePath}`;
-          component
-            .then((e: JSX.Element) => {
-              document.getElementById(suspenseId)?.replaceWith(transpile(e));
-            })
-            .catch((error: Error) => {
-              document.getElementById(suspenseId)?.replaceWith(transpile(suspense.error(error)));
-            });
-          return <div id={suspenseId}>{suspense.loading()}</div>;
-        };
-        return { component: SuspenseComponent, params: matchedParams };
-      }
-      return { component: component, params: matchedParams };
-    }
-  }
-  return { component: suspense.notFound, params: {} };
-}
-
-export async function renderPage<T = any>(this: CaratsRenderContext, component: CaratsComponent<T>, props: T): Promise<string> {
-  return transpile(component(props));
 }
---
+++ packages/render/src/index.tsx
@@ -0,0 +1,55 @@
+import { matchRoute } from "@carats/url";
+import { transpile } from "jjsx";
+
+interface CaratsComponent<T = any> extends JSX.FunctionComponent<T> {
+  ssp?: string;
+  defaultProps?: T
+}
+
+export interface CaratsRenderContext {
+  inAppRouting?: boolean;
+  routes: Record<string, CaratsComponent>
+  suspense: {
+    loading: () => JSX.Element;
+    error: (error: Error) => JSX.Element;
+    notFound: () => JSX.Element;
+  }
+}
+
+interface PageComponentResult {
+  component: CaratsComponent<any>;
+  params: Record<string, string>;
+}
+
+export function getPageComponent(this: CaratsRenderContext, path: string): PageComponentResult {
+
+  const { routes, suspense } = this;
+
+
+  for (const routePath in routes) {
+    const component = routes[routePath];
+    const matchedParams = matchRoute(routePath, path);
+    if (matchedParams) {
+      if (component instanceof Promise) {
+        const SuspenseComponent = () => {
+          const suspenseId = `carats-suspense-${routePath}`;
+          component
+            .then((e: JSX.Element) => {
+              document.getElementById(suspenseId)?.replaceWith(transpile(e));
+            })
+            .catch((error: Error) => {
+              document.getElementById(suspenseId)?.replaceWith(transpile(suspense.error(error)));
+            });
+          return <div id={suspenseId}>{suspense.loading()}</div>;
+        };
+        return { component: SuspenseComponent, params: matchedParams };
+      }
+      return { component: component, params: matchedParams };
+    }
+  }
+  return { component: suspense.notFound, params: {} };
+}
+
+export async function renderPage<T = any>(this: CaratsRenderContext, component: CaratsComponent<T>, props: T): Promise<string> {
+  return transpile(component(props));
+}
