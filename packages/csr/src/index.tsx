5f8cba6d36990eaf9eb668350778ec104b3ca881
Ufuk Bakan <println.ufukbakan@gmail.com>
2026-04-02 13:06:55 +0300

    refactor: sync facet mounting
    rename builder api
    support popstate client rendering

--- packages/csr/src/index.tsx
+++ packages/csr/src/index.tsx
@@ -21,57 +21,69 @@ if (!window.ssp) {
   }
 }
 
-export default async function BuildCarats(facets: Facets) {
-  init()
-  const { suspense, inAppRouting = true } = facets
+let _facets: Facets = {} as Facets;
+
+export async function clientRender() {
+  const { suspense } = _facets
   suspense.loading = suspense.loading ?? (() => <>💎 Loading...</>)
   suspense.error = suspense.error ?? ((error: Error) => <>💎 Error: {error.message}</>)
   suspense.notFound = suspense.notFound ?? (() => <>💎 Not Found</>)
 
-  async function clientRender() {
-    const url = location.pathname + location.search
-    const loaderTimer = setTimeout(() => document.getElementById("loading-indicator")?.classList.remove("hide"), 250)
-    await clearHydrations()
-    try {
-      const { component } = getPageComponent.call(facets, location.href)
-      let props = component.defaultProps
-      if (component.burnished && window.ssp.for !== url) {
-          const sspUrl = `/culet${url}`
-          props = await fetch(sspUrl).then(r => r.json())
-          window.ssp.data = props
-          window.ssp.for = url
-      } else {
-        props = window.ssp.data
-      }
-      const html = await renderPage.call(facets, component, props)
-      document.getElementById("app")!.innerHTML = html
-    } catch (error) {
-      document.getElementById("app")!.innerHTML = transpile(suspense.error(error as Error))
-    } finally {
-      clearTimeout(loaderTimer)
-      window.dispatchEvent(new Event("load"))
-      document.getElementById("loading-indicator")?.classList.add("hide")
+  const url = location.pathname + location.search
+  const loaderTimer = setTimeout(() => document.getElementById("loading-indicator")?.classList.remove("hide"), 250)
+  await clearHydrations()
+  try {
+    const { component } = getPageComponent.call(_facets, location.href)
+    let props = component.defaultProps
+    if (component.burnished && window.ssp.for !== url) {
+      const sspUrl = `/culet${url}`
+      props = await fetch(sspUrl).then(r => r.json())
+      window.ssp.data = props
+      window.ssp.for = url
+    } else {
+      props = window.ssp.data
     }
+    const html = await renderPage.call(_facets, component, props)
+    document.getElementById("app")!.innerHTML = html
+  } catch (error) {
+    document.getElementById("app")!.innerHTML = transpile(suspense.error(error as Error))
+  } finally {
+    clearTimeout(loaderTimer)
+    window.dispatchEvent(new Event("load"))
+    document.getElementById("loading-indicator")?.classList.add("hide")
   }
+}
+
+export function goTo(url: string) {
+  const targetUrl = new URL(url, location.origin)
+  if (targetUrl.origin !== location.origin) {
+    window.location.href = url
+    return
+  }
+  history.pushState(null, "", url)
+  clientRender()
+}
+
+export function mount(facets: Facets) {
+  init()
+  _facets = facets
+  const { inAppRouting = true } = facets
 
   if (inAppRouting) {
     window.addEventListener("load", () => {
       const anchors = document.querySelectorAll<HTMLAnchorElement>("a")
       anchors.forEach((anchor) => {
         if (anchor._isHandled) return
+        const targetUrl = new URL(anchor.href, location.origin)
+        if (targetUrl.origin !== location.origin || anchor.download) return
         anchor.addEventListener("click", (event) => {
-          history.pushState(null, "", anchor.href)
-          const targetUrl = new URL(anchor.href)
-          if (targetUrl.origin !== location.origin || anchor.download) return
           event.preventDefault()
+          history.pushState(null, "", anchor.href)
           clientRender()
         })
         anchor._isHandled = true
       })
     })
-  }
-
-  return {
-    clientRender,
+    window.addEventListener("popstate", clientRender)
   }
 }
